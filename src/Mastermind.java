21635ac0092f3096cf2782f0de763e3ecbe7d52f
Derya Sopher <deryasopher@gmail.com>
2014-07-20 22:42:47 -0400

    wrote generateSolution()

--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,4 +1,6 @@
+import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Mastermind {
@@ -7,7 +9,26 @@ public class Mastermind {
 	
 	private static ArrayList<Peg> generateSolution() {
 		ArrayList<Peg> solution = new ArrayList<Peg>(PATTERN_LENGTH);
-		//TODO: add four Pegs to solution
+		
+		Random random1 = new Random();
+		
+		for(int i = 0; i < PATTERN_LENGTH; i++){
+			int rand = random1.nextInt(6);
+			
+			if(rand == 0){
+				solution.add(new Peg(Color.white));
+			}else if(rand == 1){
+				solution.add(new Peg(Color.orange));
+			}else if(rand == 2){
+				solution.add(new Peg(Color.red));
+			}else if(rand == 3){
+				solution.add(new Peg(Color.green));
+			}else if(rand == 4){
+				solution.add(new Peg(Color.yellow));
+			}else if(rand == 5){
+				solution.add(new Peg(Color.blue));
+			}
+		}
 		
 		return solution;
 	}
@@ -27,8 +48,6 @@ public class Mastermind {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		System.out.println("Welcome to Mastermind!");
 		
 		ArrayList<Peg> solution = generateSolution();
