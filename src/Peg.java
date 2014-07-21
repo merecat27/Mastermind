5f67e43b9be77c5f911333aa89865935f83e6831
Derya Sopher <deryasopher@gmail.com>
2014-07-20 20:44:25 -0400

    added Peg class

---
+++ src/Peg.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+
+public class Peg{
+
+	private Color pegColor;
+	
+	public Peg(Color pegColor){
+		this.pegColor = pegColor;
+	}
+	
+	public Color getPegColor(){
+		return pegColor;
+	}
+	
+}
