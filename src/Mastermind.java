a6a3684ae39dd20576e801d591b4fc743ca3ca3a
Revan Sopher <rsopher@gmail.com>
2014-07-20 22:11:42 -0400

    Logic skeleton.

--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,11 +1,36 @@
+import java.util.ArrayList;
+
 
 public class Mastermind {
+	
+	private final static int PATTERN_LENGTH = 4;
+	
+	private static ArrayList<Peg> generateSolution() {
+		ArrayList<Peg> solution = new ArrayList<Peg>(PATTERN_LENGTH);
+		//TODO: add four Pegs to solution
+		
+		return solution;
+	}
+	
+	private static ArrayList<Peg> getUserGuess() {
+		ArrayList<Peg> guess = new ArrayList<Peg>(PATTERN_LENGTH);
+		//TODO: get user's guess, store in guess
+		
+		return guess;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Welcome to Mastermind!");
 		
+		ArrayList<Peg> solution = generateSolution();
+		ArrayList<Peg> guess;
+		do {
+			guess = getUserGuess();
+		} while(!patternsMatch(solution, guess));
+		
+		System.out.println("Game Over!");
 	}
 
 }
